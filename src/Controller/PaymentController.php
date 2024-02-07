74c79d76ad88779f57b3096718d0f461db4eec4f
François <gofrev@gmail.com>
2024-02-07 09:23:43 +0100

    Ajout d'un webhook

--- src/Controller/PaymentController.php
+++ src/Controller/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Stripe\StripeClient;
 use App\Form\PaymentType;
 use App\Entity\Transaction;
 use App\Service\StripeService;
@@ -80,9 +81,41 @@ class PaymentController extends AbstractController
     #[Route('/success', name: 'payment_success')]
     public function success(): Response
     {
-        return $this->render('payment/success.html.twig', [
-            'controller_name' => 'PaymentController',
-        ]);
+        $stripe = new StripeClient($this->getParameter('STRIPE_API_KEY_SECRET'));
+
+        // This is your Stripe CLI webhook secret for testing your endpoint locally.
+        $endpoint_secret = 'whsec_780e28e45e28f9e9fec1e54692a175cee0ba954ffa20297c9110c2d21031ddc5';
+
+        $payload = @file_get_contents('php://input');
+        $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
+        $event = null;
+
+        try {
+        $event = \Stripe\Webhook::constructEvent(
+            $payload, $sig_header, $endpoint_secret
+        );
+        } catch(\UnexpectedValueException $e) {
+        // Invalid payload
+        http_response_code(400);
+        exit();
+        } catch(\Stripe\Exception\SignatureVerificationException $e) {
+        // Invalid signature
+        http_response_code(400);
+        exit();
+        }
+
+        // Handle the event
+        switch ($event->type) {
+        case 'payment_intent.succeeded':
+            $paymentIntent = $event->data->object;
+        // ... handle other event types
+        default:
+            echo 'Received unknown event type ' . $event->type;
+        }
+
+        http_response_code(200);
+
+        return $this->render('payment/success.html.twig');
     }
     #[Route('/cancel', name: 'payment_cancel')]
     public function cancel(): Response
